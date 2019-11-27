bd8101f29c17ec5be677c6fff89870e4ce1527cc
James Harper <jharper@nanthealth.com>
2019-11-27 12:21:14 -0600

    Update IpInfoJSONReply.java

--- src/main/java/com/leaddore/godaddydnsupdater/IpInfoJSONReply.java
+++ src/main/java/com/leaddore/godaddydnsupdater/IpInfoJSONReply.java
@@ -5,180 +5,181 @@ package com.leaddore.godaddydnsupdater;
  */
 public class IpInfoJSONReply {
 
-	/** The ip. */
-	private String ip;
+    /** The ip. */
+    private String ip;
 
-	/** The host name. */
-	private String hostName;
+    /** The host name. */
+    private String hostName;
 
-	/** The city. */
-	private String city;
+    /** The city. */
+    private String city;
 
-	/** The region. */
-	private String region;
+    /** The region. */
+    private String region;
 
-	/** The country. */
-	private String country;
+    /** The country. */
+    private String country;
 
-	/** The loc. */
-	private String loc;
+    /** The loc. */
+    private String loc;
 
-	/** The org. */
-	private String org;
+    /** The org. */
+    private String org;
 
-	/** The postal. */
-	private String postal;
+    /** The postal. */
+    private String postal;
 
-	/**
-	 * Gets the ip.
-	 *
-	 * @return the ip
-	 */
-	public String getIp() {
-		return ip;
-	}
+    /**
+     * Gets the ip.
+     *
+     * @return the ip
+     */
+    public String getIp() {
+        return ip;
+    }
 
-	/**
-	 * Sets the ip.
-	 *
-	 * @param ip
-	 *            the new ip
-	 */
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
+    /**
+     * Sets the ip.
+     *
+     * @param ip
+     *            the new ip
+     */
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
 
-	/**
-	 * Gets the host name.
-	 *
-	 * @return the host name
-	 */
-	public String getHostName() {
-		return hostName;
-	}
+    /**
+     * Gets the host name.
+     *
+     * @return the host name
+     */
+    public String getHostName() {
+        return hostName;
+    }
 
-	/**
-	 * Sets the host name.
-	 *
-	 * @param hostName
-	 *            the new host name
-	 */
-	public void setHostName(String hostName) {
-		this.hostName = hostName;
-	}
+    /**
+     * Sets the host name.
+     *
+     * @param hostName
+     *            the new host name
+     */
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
 
-	/**
-	 * Gets the city.
-	 *
-	 * @return the city
-	 */
-	public String getCity() {
-		return city;
-	}
+    /**
+     * Gets the city.
+     *
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
 
-	/**
-	 * Sets the city.
-	 *
-	 * @param city
-	 *            the new city
-	 */
-	public void setCity(String city) {
-		this.city = city;
-	}
+    /**
+     * Sets the city.
+     *
+     * @param city
+     *            the new city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	/**
-	 * Gets the region.
-	 *
-	 * @return the region
-	 */
-	public String getRegion() {
-		return region;
-	}
+    /**
+     * Gets the region.
+     *
+     * @return the region
+     */
+    public String getRegion() {
+        return region;
+    }
 
-	/**
-	 * Sets the region.
-	 *
-	 * @param region
-	 *            the new region
-	 */
-	public void setRegion(String region) {
-		this.region = region;
-	}
+    /**
+     * Sets the region.
+     *
+     * @param region
+     *            the new region
+     */
+    public void setRegion(String region) {
+        this.region = region;
+    }
 
-	/**
-	 * Gets the country.
-	 *
-	 * @return the country
-	 */
-	public String getCountry() {
-		return country;
-	}
+    /**
+     * Gets the country.
+     *
+     * @return the country
+     *         Ping
+     */
+    public String getCountry() {
+        return country;
+    }
 
-	/**
-	 * Sets the country.
-	 *
-	 * @param country
-	 *            the new country
-	 */
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    /**
+     * Sets the country.
+     *
+     * @param country
+     *            the new country
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-	/**
-	 * Gets the loc.
-	 *
-	 * @return the loc
-	 */
-	public String getLoc() {
-		return loc;
-	}
+    /**
+     * Gets the loc.
+     *
+     * @return the loc
+     */
+    public String getLoc() {
+        return loc;
+    }
 
-	/**
-	 * Sets the loc.
-	 *
-	 * @param loc
-	 *            the new loc
-	 */
-	public void setLoc(String loc) {
-		this.loc = loc;
-	}
+    /**
+     * Sets the loc.
+     *
+     * @param loc
+     *            the new loc
+     */
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
 
-	/**
-	 * Gets the org.
-	 *
-	 * @return the org
-	 */
-	public String getOrg() {
-		return org;
-	}
+    /**
+     * Gets the org.
+     *
+     * @return the org
+     */
+    public String getOrg() {
+        return org;
+    }
 
-	/**
-	 * Sets the org.
-	 *
-	 * @param org
-	 *            the new org
-	 */
-	public void setOrg(String org) {
-		this.org = org;
-	}
+    /**
+     * Sets the org.
+     *
+     * @param org
+     *            the new org
+     */
+    public void setOrg(String org) {
+        this.org = org;
+    }
 
-	/**
-	 * Gets the postal.
-	 *
-	 * @return the postal
-	 */
-	public String getPostal() {
-		return postal;
-	}
+    /**
+     * Gets the postal.
+     *
+     * @return the postal
+     */
+    public String getPostal() {
+        return postal;
+    }
 
-	/**
-	 * Sets the postal.
-	 *
-	 * @param postal
-	 *            the new postal
-	 */
-	public void setPostal(String postal) {
-		this.postal = postal;
-	}
+    /**
+     * Sets the postal.
+     *
+     * @param postal
+     *            the new postal
+     */
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
 
 }
